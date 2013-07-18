a248122bbf4b04cdbeea54b0f810b8236c48aa99
jdrogers73 <james.rogers.web@gmail.com>
2013-07-18 14:15:32 -0400

    MapV2: Adding second activity
    Map will launch from second activity using Google's
    default test launch method.
    Step 1. Modify manifest
    Step 2. Create layout
    Step 3. Create mapActivity
    Step 4. Create button on main to initiate mapActivity

---
+++ MapV2/src/edu/rogers/mapv2/MapActivity.java
@@ -0,0 +1,16 @@
+package edu.rogers.mapv2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class MapActivity extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+	}
+}
