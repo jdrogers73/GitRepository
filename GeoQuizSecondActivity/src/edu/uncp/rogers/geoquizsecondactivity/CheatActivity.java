b0002b7ab5f37f1f55ed0263fca7a1711d34d3cd
jdrogers73 <james.rogers.web@gmail.com>
2013-07-18 12:17:34 -0400

    Deleted 5 student projects
    Imported for phone interview. Are no longer needed.

--- GeoQuizSecondActivity/src/edu/uncp/rogers/geoquizsecondactivity/CheatActivity.java
+++ GeoQuizSecondActivity/src/edu/uncp/rogers/geoquizsecondactivity/CheatActivity.java
@@ -8,16 +8,18 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CheatActivity extends Activity
 {
 	private Button mShowAnswerButton;
 	public static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
 	public static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";
+	private final String KEY_ANSWER_SHOWN = "com.rogers.geoquizsecondactivity.answer_shown";
+	private final String KEY_ANSWER_IS_TRUE = "com.rogers.geoquizsecondactivity.answer_is_true";
 	private static final String TAG = "QuizActivity";
 	private boolean mAnswerIsTrue;
 	private TextView mAnswerTextView;
-	private Button mShowAnswer;
 	private boolean mShowAnswerPressed;
 	
 	private void setAnswerShownResult(boolean isAnswerShown)
@@ -35,12 +37,29 @@ public class CheatActivity extends Activity
 		setContentView(R.layout.activity_cheat);
 		
 		mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
-		mShowAnswerPressed = savedInstanceState.getBoolean(EXTRA_ANSWER_SHOWN);
+		//mShowAnswerPressed = savedInstanceState.getBoolean(EXTRA_ANSWER_SHOWN);
 		
 		mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
 		
-		//if(!mShowAnswerPressed)
-		setAnswerShownResult(false);
+		//Chapter 5 challenge 1: I added the if/else to preserve record of Show Answer being pressed. 
+		if(savedInstanceState != null)
+		{
+			setAnswerShownResult(true);
+			
+			//Preserve the displayed answer in the event of orientation change
+			if (savedInstanceState.getBoolean(KEY_ANSWER_IS_TRUE))
+			{
+				mAnswerTextView.setText(R.string.true_button);
+			}
+			else
+			{
+				mAnswerTextView.setText(R.string.false_button);
+			}
+		}
+		else
+		{
+			setAnswerShownResult(false);
+		}
 		
 		mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
 		mShowAnswerButton.setOnClickListener(new View.OnClickListener()
@@ -57,7 +76,6 @@ public class CheatActivity extends Activity
 					mAnswerTextView.setText(R.string.false_button);
 				}
 				setAnswerShownResult(true);
-				
 			}
 		});
 	}
@@ -67,7 +85,8 @@ public class CheatActivity extends Activity
     {
     	super.onSaveInstanceState(savedInstanceState);
     	Log.i(TAG,"onSaveInstanceState");
-    	savedInstanceState.putBoolean(EXTRA_ANSWER_SHOWN, mShowAnswerPressed);
+    	savedInstanceState.putBoolean(KEY_ANSWER_SHOWN, mShowAnswerPressed);
+    	savedInstanceState.putBoolean(KEY_ANSWER_IS_TRUE, mAnswerIsTrue);
     }
 	
 }
--- GeoQuizSecondActivity/src/edu/uncp/rogers/geoquizsecondactivity/QuizActivity.java
+++ GeoQuizSecondActivity/src/edu/uncp/rogers/geoquizsecondactivity/QuizActivity.java
@@ -14,16 +14,36 @@ import edu.uncp.rogers.geoquizsecondactivity.R;
 
 public class QuizActivity extends Activity 
 {
-	
+	//user answers "true" by using this button
 	private Button mTrueButton;
-	private Button mFalseButton;
-	private Button mNextButton;
-	private Button mCheatButton;
-	private TextView mQuestionTextView;
-	private static final String TAG = "QuizActivity";
-	private static final String KEY_INDEX = "index";
-	private static Boolean mIsCheater = false;
 	
+	//user answers "false" by using this button
+	private Button mFalseButton;
+	
+	//user generates next question by using this button
+	private Button mNextButton;
+	
+	//user can go to the Cheat screen using this button
+	private Button mCheatButton;
+	
+	//used to display the question
+	private TextView mQuestionTextView;
+	
+	//Will identify the activity in the system log 
+	private static final String TAG = "QuizActivity";
+	
+	//used to store an extra value in a Bundle
+	private static final String KEY_INDEX = "index";
+	
+	//After user sees the answer by using the cheat feature, the user is labeled a cheater;
+	//This string will be used as a key to preserve the cheater status on device rotation by saving the
+	//cheater status to the savedInstanceState object. 
+	private final String KEY_IS_CHEATER = "edu.rogers.geoquizsecondactivity.key_is_cheater";
+	
+	//stores true is the user cheated on the question or false otherwise.
+	private static boolean mIsCheater = false;
+	
+	//creates T/F questions and the answers; uses the TrueFalse class
 	private TrueFalse[] mQuestionBank = new TrueFalse[]
 	{
 			new TrueFalse(R.string.question_oceans, true),
@@ -33,73 +53,61 @@ public class QuizActivity extends Activity
 			new TrueFalse(R.string.question_asia, true),
 	};
 	
+	//questions are saved in an array; this variable saves the index of the current question
 	private int mCurrentIndex = 0;
-
-	private void updateQuestion()
-	{
-		//Log.d(TAG,"Updating question text for question #" + mCurrentIndex, new Exception());
-		int question = mQuestionBank[mCurrentIndex].getQuestion();
-		mQuestionTextView.setText(question);
-	}
 	
-	private void checkAnswer(boolean userPressedTrue)
-	{
-		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
-		
-		int messageResID = 0;
-		
-		if(mIsCheater)
-		{
-			messageResID = R.string.judgment_toast;
-		}
-		else
-		{
-			if (userPressedTrue == answerIsTrue)
-			{
-				messageResID = R.string.correct_toast;
-			}
-			else
-			{
-				messageResID = R.string.incorrect_toast;
-			}
-		}
-		Toast.makeText(this, messageResID, Toast.LENGTH_SHORT).show();
-	}
-	
+	//override onCreate
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data)
-	{
-		if (data == null)
-			return;
-		mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
-	}
-	
-    @Override
+	/*OnCreate - will create the user interface defined in activity_quiz
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 * 
+	 * Bundle savedInstanceState - the state of the activity before onDestroy() is called
+	 * 
+	 */
     protected void onCreate(Bundle savedInstanceState) 
     {
-        super.onCreate(savedInstanceState);
+        //call to the Activity class to create the View Hierarchy for this activity
+		super.onCreate(savedInstanceState);
+		
+		//note the current progress of the program in the Log at level Debug
         Log.d(TAG, "onCreate() called");
+        
+        //Inflate the activity_quiz layout
         setContentView(R.layout.activity_quiz);
         
+        //set reference to the TextView object
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
         
+        //set reference to the button that will display the word True
         mTrueButton = (Button)findViewById(R.id.true_button);
+        
+        //setup button to respond to click
         mTrueButton.setOnClickListener(new OnClickListener()
         {
         	@Override
         	public void onClick (View v)
         	{
+        		//Call the check answer method to compare the users click to the answer stored in the
+        		//question bank array
         		//Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
         		checkAnswer(true);
         	}
         });
         
+        //set reference to the false button; Android will look for a child view in the View tree bearing the given ID.
+        //If the view is not found, null will be returned.
+        //Android then returns the View and the View object is then converted into a Button object
         mFalseButton = (Button)findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new OnClickListener()
+        
+      //setup button to respond to click
+        mFalseButton.setOnClickListener(new View.OnClickListener()
         {
         	@Override
         	public void onClick(View v)
         	{
+        		//Call the check answer method to compare the users click to the answer stored in the
+        		//question bank array
         		//Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
         		checkAnswer(false);
         	}
@@ -111,8 +119,11 @@ public class QuizActivity extends Activity
         	@Override
         	public void onClick(View v)
         	{
+        		//calculate the index of the question to be displayed
         		mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        		//reset mIsCheater since cheating status exists for only the question cheated
         		mIsCheater = false;
+        		//retrieve the latest question
         		updateQuestion();
         	}
         });
@@ -134,10 +145,61 @@ public class QuizActivity extends Activity
         if(savedInstanceState != null)
         {
         	mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        	//mIsCheater = savedInstanceState.getBoolean(KEY_IS_CHEATER);
         }
         
         updateQuestion();
     }
+	
+	private void updateQuestion()
+	{
+		//Log.d(TAG,"Updating question text for question #" + mCurrentIndex, new Exception());
+		
+		//get the index of the current question
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		
+		//display the question on the screen by updating the TextView object
+		mQuestionTextView.setText(question);
+	}
+	
+	private void checkAnswer(boolean userPressedTrue)
+	{
+		
+		//get answer to current question
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		
+		//R file ID of string matching outcome of answer
+		int messageResID = 0;
+		
+		//if user cheated, display cheating message
+		if(mIsCheater)
+		{
+			messageResID = R.string.judgment_toast;
+		}
+		//if user guessed correctly print correct to screen; else print false
+		else
+		{
+			if (userPressedTrue == answerIsTrue)
+			{
+				messageResID = R.string.correct_toast;
+			}
+			else
+			{
+				messageResID = R.string.incorrect_toast;
+			}
+		}
+		
+		//create the toast and show it.
+		Toast.makeText(this, messageResID, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if (data == null)
+			return;
+		mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+	}
     
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState)
@@ -145,6 +207,7 @@ public class QuizActivity extends Activity
     	super.onSaveInstanceState(savedInstanceState);
     	Log.i(TAG,"onSaveInstanceState");
     	savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    	//savedInstanceState.putBoolean(KEY_IS_CHEATER, mIsCheater);
     }
 
     @Override
